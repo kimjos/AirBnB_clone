42171748dfd86d9c16e28e4d9e375f4f4df31abc
JoseMarulanda <josejumarumol@gmail.com>
2019-11-13 02:51:35 +0000

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Class Amenity that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Class to save the Amenity
+    """
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of the amenity"""
+        super().__init__(**kwargs)
